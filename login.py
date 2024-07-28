e5ae171888a15355d58271fa2d811dfb53f77a87
ins-tukumin <116026504+ins-tukumin@users.noreply.github.com>
2024-07-28 15:00:10 +0900

    Update login.py

--- login.py
+++ login.py
@@ -27,8 +27,8 @@ def load_participants(file_path):
         pass  # ファイルが見つからない場合でもエラーを表示しない
 
     # リストにないIDには "xxxx" を割り当てる
-    for user_id in participants.keys():
-        if participants[user_id] == "":
+    for user_id, group in participants.items():
+        if not group:
             participants[user_id] = "xxxx"
     
     return participants
@@ -36,34 +36,16 @@ def load_participants(file_path):
 # Streamlitアプリケーションのレイアウト
 st.title("ログインページ")
 
-# セッション状態を初期化
-if 'logged_in' not in st.session_state:
-    st.session_state.logged_in = False
-    st.session_state.user_id = ""
-    st.session_state.group = ""
-
 # 参加者のリストを読み込む
 participants = load_participants('group_assignment.txt')
 
-if st.session_state.logged_in:
-    st.success(f"ログイン済み: {st.session_state.user_id}")
-    group_url = group_urls.get(st.session_state.group)
-    if group_url:
-        group_url_with_id = f"{group_url}?user_id={st.session_state.user_id}&group={st.session_state.group}"
+user_id = st.text_input("学籍番号を半角で入力してください")
+if st.button("ログイン"):
+    if user_id:
+        group = participants.get(user_id, "xxxx")
+        group_url = group_urls.get(group, group_urls["xxxx"])
+        group_url_with_id = f"{group_url}?user_id={user_id}&group={group}"
+        #st.success(f"ログイン成功: {user_id}")
         st.markdown(f'こちらのURLをクリックしてください: <a href="{group_url_with_id}" target="_blank">リンク</a>', unsafe_allow_html=True)
     else:
-        st.write("対応するグループURLが見つかりません。")
-else:
-    user_id = st.text_input("学籍番号を半角で入力してください")
-    if st.button("ログイン"):
-        if user_id:
-            group = participants.get(user_id)
-            if group:
-                st.session_state.logged_in = True
-                st.session_state.user_id = user_id
-                st.session_state.group = group
-                # st.success(f"ログイン成功: {user_id}")
-                st.experimental_rerun()
-            else:
-                st.write("無効なIDです。もう一度お試しください。")
-
+        st.write("学籍番号を入力してください。")
